65102a9186c1b48464c97045bb464fdc54005063
Irfando <irfandoaritonang86@gmail.com>
2023-08-15 22:42:27 +0700

    finish

--- app/src/main/java/com/example/profilecardapp/MainActivity.kt
+++ app/src/main/java/com/example/profilecardapp/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,8 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,20 +49,103 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun NameCardApp() {
     Column (
-        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
-    ) {
-        val image = painterResource(R.drawable.android_logo_baru)
-        Image(
-            painter = image,
-            contentDescription = null,
-            modifier = Modifier
-                .height(200.dp)
-                .width(200.dp)
-        )
-        Text(
-            text = stringResource(R.string.irfando_aritonang),
-            fontSize = 36.sp
-        )
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(color = colorResource(id = R.color.green)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        //verticalArrangement = Arrangement.Center
+    ){
+            Column(
+                modifier = Modifier
+                    .padding(top = 200.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+                //verticalArrangement = Arrangement.Center
+            ) {
+                val image = painterResource(R.drawable.android_logo_baru)
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(160.dp)
+                        .width(160.dp)
+                )
+                Text(
+                    text = stringResource(R.string.irfando_aritonang),
+                    modifier = Modifier
+                        .padding(top = 12.dp),
+                    fontSize = 36.sp
+                )
+                Text(
+                    text = stringResource(R.string.title),
+                    fontWeight = FontWeight.Bold,
+                    //color = Color.Green
+                    style = TextStyle(color = colorResource(R.color.green_dark)),
+                    fontSize = 16.sp
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(top = 240.dp)
+                    .padding(start = 20.dp),
+                horizontalAlignment = Alignment.Start
+                //verticalArrangement = Arrangement.Center
+            ) {
+                Row (modifier = Modifier
+                    .padding(bottom = 5.dp))
+                {
+                    Image(
+                        painter = painterResource(R.drawable.whatsapp_100),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(30.dp)
+                            .width(30.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.no_Hp),
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                            .padding(start = 10.dp),
+                        fontSize = 16.sp,
+                    )
+                }
+                Row (modifier = Modifier
+                    .padding(bottom = 5.dp))
+                {
+                    Image(
+                        painter = painterResource(R.drawable.instagram_100),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(30.dp)
+                            .width(30.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.instagram),
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                            .padding(start = 10.dp),
+                        fontSize = 16.sp,
+                    )
+                }
+                Row (modifier = Modifier
+                    .padding(bottom = 5.dp))
+                {
+                    Image(
+                        painter = painterResource(R.drawable.email_100),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(30.dp)
+                            .width(30.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.email),
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                            .padding(start = 10.dp),
+                        fontSize = 16.sp,
+                    )
+                }
+            }
     }
 
 }
